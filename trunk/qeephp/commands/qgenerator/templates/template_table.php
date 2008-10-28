9aaff1921b0ffe65163b7ccbd2a14ae2d2b87f20
dualface <dualface@08fa1087-9735-0410-867b-77273fa98611>
2008-10-28 18:41:55 +0000

    * 同步代码

--- trunk/qeephp/commands/qgenerator/templates/template_table.php
+++
@@ -1,8 +0,0 @@
-<?php echo '<?php'; ?>
-
-
-class <?php echo $class_name; ?> extends QDB_Table
-{
-    public $table_name = '<?php echo $table_name; ?>';
-    public $pk = '<?php echo implode(', ', $pk); ?>';
-}
